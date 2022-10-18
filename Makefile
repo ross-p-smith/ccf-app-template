9a31d22a905068723d80fcfaa3dfc106d2b05266
ross-p-smith <rosmith@microsoft.com>
2022-10-18 19:26:07 +0000

    Installing CCF inside devcontainer

---
+++ Makefile
@@ -0,0 +1,13 @@
+SHELL := /bin/bash
+
+.PHONY: help install
+.DEFAULT_GOAL := help
+CCF_DIR=CCF
+
+help: ## 💬 This help message :)
+	@echo -e "\e[34m$@\e[0m" || true
+	@grep -E '[a-zA-Z_-]+:.*?## .*$$' $(firstword $(MAKEFILE_LIST)) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
+
+install: ## 🔨 Install the CCF framework
+	@echo -e "\e[34m$@\e[0m" || true
+	@scripts/install.sh
---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+	#!/bin/bash
+set -euo pipefail
+
+declare CCF_DIR=CCF
+
+git -C ${CCF_DIR} pull || git clone --depth 1 https://github.com/microsoft/CCF.git ${CCF_DIR}
+
+./${CCF_DIR}/getting_started/setup_vm/run.sh ./${CCF_DIR}/getting_started/setup_vm/app-dev.yml
+
+CCF_VERSION=$(curl -ILs -o /dev/null -w %{url_effective} https://github.com/microsoft/CCF/releases/latest | sed 's/^.*ccf-//')
+wget https://github.com/microsoft/CCF/releases/download/ccf-${CCF_VERSION}/ccf_${CCF_VERSION}_amd64.deb
